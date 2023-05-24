0c3597218767baeef2326d965165f1967e55bac0
Nan0MK <nan0mk@outlook.com>
2023-05-24 03:27:36 -0600

    stuff

--- source/app.d
+++ source/app.d
@@ -8,7 +8,7 @@ void main(){
 
 	writeln("Survive The Lava Wave!");
 
-    auto the_ground = new SquareTile(130, 480, 550, 30, Colors.RED);
+    auto the_ground = new SquareTile(141, 480, 550, 50, Colors.RED);
 
 
 	// call this before using raylib
@@ -19,8 +19,11 @@ void main(){
 		
         BeginDrawing();
         ClearBackground(Colors.RAYWHITE);
+
         DrawText("Survive The Lava Wave!", 400, 300, 28, Colors.BLACK);
         the_ground.draw_me();
+        DrawCircle(the_ground.edge_top_x, the_ground.edge_bot, 30, Colors.BROWN);
+
         EndDrawing();
     }
     CloseWindow();
--- source/entities.d
+++ source/entities.d
@@ -11,12 +11,30 @@ class SquareTile : Entity{
     int size_x;
     int size_y;
 
+    int edge_top;
+    int edge_top_x;
+
+    int edge_bot;
+    int edge_bot_x;
+
+    int edge_left;
+    int edge_right;
+
     this(int xIn, int yIn, int sizeIn_x, int sizeIn_y, Colors colorIn){
         pos_x = xIn;
         pos_y = yIn;
         size_x = sizeIn_x;
         size_y = sizeIn_y;
         tile_color = colorIn;
+
+        edge_top = pos_y - size_y;
+        edge_top_x = pos_x + pos_x;
+
+        edge_bot = pos_y + size_y;
+        edge_bot_x = pos_x + pos_x;
+
+        edge_left = -size_x;
+        edge_right = size_x;
     }
 
     void draw_me(){
