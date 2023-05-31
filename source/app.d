da29b707323107aac7e9f9fa15f07e88afc78e87
Nan0MK <nan0mk@outlook.com>
2023-05-30 20:33:02 -0600

    Added multi-object collision

--- source/app.d
+++ source/app.d
@@ -12,7 +12,18 @@ void main(){
 
 	writeln("Survive The Lava Wave!");
 
-    auto the_ground = new SquareTile(140, 480, 70, 70, Colors.RED);
+    auto tile0 = new SquareTile(140, 480, 70, 70, Colors.RED);
+    auto tile1 = new SquareTile(tile0.pos_x + 140, 480, 70, 70, tile0.tile_color);
+    auto tile2 = new SquareTile(tile1.pos_x + 140, 480, 70, 70, tile0.tile_color);
+    auto tile3 = new SquareTile(tile2.pos_x + 140, 480, 70, 70, tile0.tile_color);
+    auto tile4 = new SquareTile(tile3.pos_x + 140, 480, 70, 70, tile0.tile_color);
+
+    SquareTile[] tileHolder;
+    tileHolder ~= tile0;
+    tileHolder ~= tile1;
+    tileHolder ~= tile2;
+    tileHolder ~= tile3;
+    tileHolder ~= tile4;
 
     auto the_player = new SquareTile(100, 400, 30, 30, Colors.GREEN);
 
@@ -27,15 +38,19 @@ void main(){
         ClearBackground(Colors.RAYWHITE);
         //Gameloop
 
-        movement_with_collision(the_player, the_ground, move_speed);
+        movement_with_collision(the_player, tileHolder, move_speed);
         //movement(the_player, move_speed);
 
         //Now to make collision stop movement
 
-        the_ground.draw_me();
+        tile0.draw_me();
+        tile1.draw_me();
+        tile2.draw_me();
+        tile3.draw_me();
+        tile4.draw_me();
         the_player.draw_me();
 
-        DrawText(show_collision_status(the_ground, the_player), 400, 300, 30, Colors.BLUE);
+        //DrawText(show_collision_status(tile0, the_player), 400, 300, 30, Colors.BLUE);
 
         //Gameloop
         EndDrawing();
--- source/utils.d
+++ source/utils.d
@@ -5,37 +5,68 @@ import raylib;
 
 import entities;
 
+//Collision checker
+bool is_colliding(SquareTile a, SquareTile b) {
+    if (a.pos_x < b.pos_x + b.size_x - 1 &&
+        a.pos_x + a.size_x > b.pos_x + 1 &&
+        a.pos_y < b.pos_y + b.size_y - 1 &&
+        a.pos_y + a.size_y > b.pos_y + 1) {
+        return true;
+    }
+    return false;
+}
+const (char)* show_collision_status(SquareTile a, SquareTile b){
+    const (char)* sout = "none".ptr;
+    if(is_colliding(a, b)){
+        sout = "True".ptr;
+    }
+    if(!is_colliding(a, b)){
+        sout = "False".ptr;
+    }
+    return sout;
+}
 
-void movement_with_collision(SquareTile movingThing, SquareTile staticThing, int movementSpeed){
+//Movement with collision
+void movement_with_collision(SquareTile movingThing, SquareTile[] staticThings, int movementSpeed){
     if(IsKeyDown(KeyboardKey.KEY_W)){
         movingThing.pos_y -= movementSpeed;
-        if(is_colliding(movingThing, staticThing)){
-            movingThing.pos_y += movementSpeed;
-        }
+        foreach (staticThing; staticThings){
+            if(is_colliding(movingThing, staticThing)){
+                movingThing.pos_y += movementSpeed;
+                break;
+            } 
+        }  
     }
-
     if(IsKeyDown(KeyboardKey.KEY_S)){
         movingThing.pos_y += movementSpeed;
-        if(is_colliding(movingThing, staticThing)){
-            movingThing.pos_y -= movementSpeed;
+        foreach (staticThing; staticThings){
+            if(is_colliding(movingThing, staticThing)){
+                movingThing.pos_y -= movementSpeed;
+                break;
+            }
         }
     }
-
     if(IsKeyDown(KeyboardKey.KEY_A)){
         movingThing.pos_x -= movementSpeed;
-        if(is_colliding(movingThing, staticThing)){
-            movingThing.pos_x += movementSpeed;
+        foreach (staticThing; staticThings){
+            if(is_colliding(movingThing, staticThing)){
+                movingThing.pos_x += movementSpeed;
+                break;
+            }
         }
     }
-
     if(IsKeyDown(KeyboardKey.KEY_D)){
         movingThing.pos_x += movementSpeed;
-        if(is_colliding(movingThing, staticThing)){
-            movingThing.pos_x -= movementSpeed;
-        }
+        foreach (staticThing; staticThings){
+            if(is_colliding(movingThing, staticThing)){
+                movingThing.pos_x -= movementSpeed;
+                break;
+            }
+        } 
     }
 }
 
+//Movement
 void movement(SquareTile movingThing, int movementSpeed){
     if(IsKeyDown(KeyboardKey.KEY_W)){
         movingThing.pos_y -= movementSpeed;
@@ -52,25 +83,4 @@ void movement(SquareTile movingThing, int movementSpeed){
     if(IsKeyDown(KeyboardKey.KEY_D)){
         movingThing.pos_x += movementSpeed;
     }
-}
-
-bool is_colliding(SquareTile a, SquareTile b) {
-    if (a.pos_x < b.pos_x + b.size_x - 1 &&
-        a.pos_x + a.size_x > b.pos_x + 1 &&
-        a.pos_y < b.pos_y + b.size_y - 1 &&
-        a.pos_y + a.size_y > b.pos_y + 1) {
-        return true;
-    }
-    return false;
-}
-
-const (char)* show_collision_status(SquareTile a, SquareTile b){
-    const (char)* sout = "none".ptr;
-    if(is_colliding(a, b) == true){
-        sout = "True".ptr;
-    }
-    if(is_colliding(a, b) == false){
-        sout = "False".ptr;
-    }
-    return sout;
 }
